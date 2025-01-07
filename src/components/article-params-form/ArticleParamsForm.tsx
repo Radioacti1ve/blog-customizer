e47e81830d6dbfb5aac2d5b98bf824a8e0453313
Maxim Kornev <radioactiive@yandex.ru>
2025-01-07 15:31:24 +0300

    opening is done

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,31 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { useState, useEffect, useRef } from 'react';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [flag, setFlag] = useState(false);
+	const asideRef = useRef<HTMLElement>(null);
+
+	const handleClick = () => {
+		setFlag((prev) => !prev);
+	};
+
+	useEffect(() => {
+		if (asideRef.current) {
+			if (flag) {
+				asideRef.current.classList.add(styles.container_open);
+			} else {
+				asideRef.current.classList.remove(styles.container_open);
+			}
+		}
+	}, [flag]);
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={flag} onClick={handleClick} />
+			<aside ref={asideRef} className={styles.container}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
